e91aa9789da47e9bcdfdfa1a1a82e113b9ef168d
OhmKhay <chestersai12@gmail.com>
2022-09-21 09:04:11 +0630

    fix: spelling

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Shan Dictonary</h1>
+        <h1>Shan Dictionary</h1>
         <Timer />
         <Optin />
         <Preloader />
